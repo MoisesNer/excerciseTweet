9962d2c51ccfab4e8ec318b868b1d4bace14c2e3
Moises Nervis <StarCommand@Moisess-iMac.local>
2021-01-27 10:43:26 -0800

    final version

--- app.js
+++ app.js
@@ -1,15 +1,46 @@
-const form = document.querySelector('#tweetForm');
+// const form = document.querySelector('#tweetForm');
 
-const usr = document.querySelector('#username');
-const txt = document.querySelector('#tweet');
+// const usr = document.querySelector('#username');
+// const txt = document.querySelector('#tweet');
 
-const tweetList = document.createElement('ul');
-document.body.appendChild(tweetList);
+// const tweetList = document.createElement('ul');
+// document.body.appendChild(tweetList);
 
-form.addEventListener('submit', function(e){
-    e.preventDefault();
-    const newTweet = document.createElement('li')
-    newTweet.innerText = usr.value + ' ' + txt.value;
-    tweetList.appendChild(newTweet);
+// form.addEventListener('submit', function(e){
+//     e.preventDefault();
+//     const newTweet = document.createElement('li')
+//     newTweet.innerHTML = '<b>' + usr.value + '</b>' + " " + txt.value;
+//     tweetList.appendChild(newTweet);
 
+// });
+
+//TEACHER'S SOLUTION
+
+const tweetForm = document.querySelector('#tweetForm');
+const tweetContainer = document.querySelector('#tweets');
+
+tweetForm.addEventListener('submit', function(event){
+    event.preventDefault();
+
+    const userName = tweetForm.elements.username;
+    const tweetInput = tweetForm.elements.tweet;
+    addTweet(userName.value, tweetInput.value);
+    //cleaning for a new submition
+    userName.value = '';
+    tweetInput.value = '';
 });
+
+const addTweet = (username,tweet) => {
+    const newTweet = document.createElement('li');
+    const bTag = document.createElement('b');
+    bTag.append(username);
+    newTweet.append(bTag);
+    newTweet.append(` - ${tweet}`);
+    tweetContainer.appendChild(newTweet);
+};
+
+// TO REMOVE WHEN CLICKING THE LIST OF TWEETS
+tweetContainer.addEventListener('click', function(e){
+    console.dir(e.target);
+    e.target.nodeName === 'LI' && e.target.removeEventListener();
+})
